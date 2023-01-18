05c17b64468659ff5c9a655ddeb52242c2b868a6
KatiaLosignore <katialos.kl@gmail.com>
2023-01-18 17:28:54 +0100

    added bonus

--- js/script.js
+++ js/script.js
@@ -4,15 +4,24 @@ const app = Vue.createApp({
   name: 'Email API',
   data() {
     return {
+        isLoading: false,
+        errorMessage: '',
         emails: []
-
     }
   },
   methods: {
     getRandomEmail() {
+        this.isLoading = true;
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
             this.emails.push(response.data.response);
+        }).catch(error => {
+            this.errorMessage = error.message;
+        }).then(() => {
+            if (this.emails.length === 10) {
+                this.isLoading = false;
+
+            }
         });
     }
   },
